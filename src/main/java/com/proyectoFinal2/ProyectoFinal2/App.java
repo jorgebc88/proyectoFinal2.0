a14310cdfbbc70fedbeb5d428127589478c2747f
jorgebc88 <jorgebc88@gmail.com>
2015-12-13 17:03:41 -0300

    13/12/2015 - Create new repository
    Creation of a new repository.

---
+++ src/main/java/com/proyectoFinal2/ProyectoFinal2/App.java
@@ -0,0 +1,13 @@
+package com.proyectoFinal2.ProyectoFinal2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
